31a9f21b2abaa6185a6101abdbd5eb136bb11dae
Alexis Tan <alexis@sen-dev.com>
2017-10-26 17:09:50 +0200

    Initial commit

---
+++ src/com/senither/redvsblue/RedVsBlue.java
@@ -0,0 +1,16 @@
+package com.senither.redvsblue;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RedVsBlue extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
